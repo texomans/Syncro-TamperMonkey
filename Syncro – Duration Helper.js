ffe82772efd9e7ce7ecd5e1add007d1f5e206071
Josh at Texoma Network Solutions <45087983+texomans@users.noreply.github.com>
2026-09-17 21:24:17 -0500

    Update version and URLs in Duration Helper script
    Updated metadata for versioning and URLs.

--- Syncro – Duration Helper.js
+++ Syncro – Duration Helper.js
@@ -5,6 +5,8 @@
 // @description  Add h:m duration presets to both the Labor Log modal and the Comment form
 // @author       Nick F - https://github.com/esperto/Syncro-TamperMonkey
 // @match        https://*.syncromsp.com/tickets/*
+// @updateURL    https://raw.githubusercontent.com/texomans/Syncro-TamperMonkey/main/Syncro%20–%20Duration%20Helper.js
+// @downloadURL  https://raw.githubusercontent.com/texomans/Syncro-TamperMonkey/main/Syncro%20–%20Duration%20Helper.js
 // @grant        none
 // ==/UserScript==
 
