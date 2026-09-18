03a660771bdb1c16d5fb288e9dd874491f7e89d8
Josh at Texoma Network Solutions <45087983+texomans@users.noreply.github.com>
2026-09-18 18:14:07 -0500

    Update script name and version for Duration Helper

--- Syncro Tickets – Duration Helper.js
+++ Syncro Tickets – Duration Helper.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Syncro – Duration Helper - Final
+// @name         Syncro Tickets – Duration Helper
 // @namespace    https://texomans.com/
-// @version      1.1.3
+// @version      1.1.4
 // @description  Add h:m duration presets to both the Labor Log modal and the Comment form
 // @author       Nick F - https://github.com/esperto/Syncro-TamperMonkey
 // @match        https://*.syncromsp.com/tickets/*
