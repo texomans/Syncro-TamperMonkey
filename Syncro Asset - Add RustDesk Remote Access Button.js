bcb5e56a23148b159a0f8b70835dee0af2fe5576
Josh at Texoma Network Solutions <45087983+texomans@users.noreply.github.com>
2026-09-17 20:51:04 -0500

    Update Syncro Asset - Add RustDesk Remote Access Button.js

--- Syncro Asset - Add RustDesk Remote Access Button.js
+++ Syncro Asset - Add RustDesk Remote Access Button.js
@@ -3,7 +3,7 @@
 // @namespace    https://www.texomans.com/
 // @version      1.1.0
 // @description  Adds RustDesk to Syncro asset pages. Uses a Remote Access dropdown when Syncro Remote Access exists, or a direct RustDesk button when it does not.
-// @match        https://*.shield.syncromsp.com/customer_assets/*
+// @match        https://*.syncromsp.com/customer_assets/*
 // @run-at       document-idle
 // @grant        none
 // ==/UserScript==
