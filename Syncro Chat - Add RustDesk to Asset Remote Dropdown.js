40fae09ee3080d4c9175a7300faeeaa22bb16f4d
Josh at Texoma Network Solutions <45087983+texomans@users.noreply.github.com>
2026-09-17 21:48:14 -0500

    Update namespace and version in user script
    Updated namespace and version for the user script.

--- Syncro Chat - Add RustDesk to Asset Remote Dropdown.js
+++ Syncro Chat - Add RustDesk to Asset Remote Dropdown.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Syncro Chat - Add RustDesk to Asset Remote Dropdown
-// @namespace    https://texomans.com/
-// @version      1.0.0
+// @namespace    https://syncromsp.com/
+// @version      1.0.1
 // @description  Adds RustDesk to the asset Remote Access dropdown on Syncro chat pages.
 // @match        https://*.syncromsp.com/chat
 // @match        https://*.syncromsp.com/chat/*
